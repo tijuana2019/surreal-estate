582def22d3b95fe280e1172a465f3d85231511b4
Jo <jo_serendipity@icloud.com>
2019-12-20 15:23:09 +0000

    added axios to dependencies

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './app.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import NavBar from './navbar';
 import Properties from './properties';
 import AddProperty from './addproperty';
@@ -15,6 +16,16 @@ function App() {
       </Switch>
     </div>
   );
+}
+
+componentDidMount() {
+  axios.get('https://dog.ceo/api/breeds/image/random')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 }
 
 export default App;
